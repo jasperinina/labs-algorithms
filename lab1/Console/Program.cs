3cb69f89a1408546467c5c3bf22551c863b2348c
Pro100sasha_HD <sasha_ablin@rambler.ru>
2024-09-09 20:54:09 +0500

    start

---
+++ lab1/Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var sw = new Stopwatch();
+        sw.Start();
+        for (int i = 0; i < 2000; i++)
+        {
+            Console.WriteLine(i);
+        }
+        sw.Stop();
+        Console.WriteLine($"Vremya: {sw.ElapsedMilliseconds}"); 
+    }
+}
